f4dfd6bff47c3dee3beb0cb9b2e168c6ffc00647
Yeshpal Singh <yash8195026-netizen>
2026-06-24 20:07:55 +0530

    Fix category lookup ignore case

--- src/main/resources/static/js/theme.js
+++ src/main/resources/static/js/theme.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const toggle = document.getElementById("themeToggle");
+//========================= DARK MODE CODE =========================
 
+    const toggle = document.getElementById("themeToggle");
     const isDark = localStorage.getItem("theme") === "dark";
 
     if(isDark){
@@ -15,24 +16,42 @@ document.addEventListener("DOMContentLoaded", () => {
     if(toggle){
 
         toggle.addEventListener("click", () => {
-
             document.documentElement.classList.toggle("dark-mode");
-
-            const darkEnabled =
-                document.documentElement.classList.contains("dark-mode");
-
+            const darkEnabled =document.documentElement.classList.contains("dark-mode");
             if(darkEnabled){
-
                 localStorage.setItem("theme","dark");
                 toggle.textContent = "☀️";
-
             }else{
-
                 localStorage.setItem("theme","light");
                 toggle.textContent = "🌙";
-
             }
         });
     }
 
+    // ================= MOBILE MENU CODE =================
+
+    const menuToggle =document.getElementById("menuToggle");
+    const mobileMenu =document.getElementById("mobileMenu");
+
+    if (menuToggle && mobileMenu) {
+        menuToggle.addEventListener("click", () => {
+
+            mobileMenu.classList.toggle("show");
+
+        });
+
+    }
+
+    const menuLinks = document.querySelectorAll(".topmenu");
+
+    menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        if (mobileMenu) {
+            mobileMenu.classList.remove("show");
+        }
+
+        });
+    }); 
+
 });
+
